2cdb232e11c89c90e6938e3c3a009cf449706f9e
Tomoya Fujii <10.fujit@fuji.waseda.jp>
2025-10-01 20:07:13 +0900

    Update Works.tsx

--- components/Works.tsx
+++ components/Works.tsx
@@ -5,6 +5,7 @@ export default function Works() {
     <Card className="md:col-span-2 p-6 shadow-lg bg-[#2C3A45]">
       <h2 className="text-2xl font-bold mb-4 text-[#CEA17A]">過去のお仕事</h2>
       <ul className="list-disc list-inside space-y-2 text-[#C7CCCF]">
+        <li>講演：特別進路講演　飛鳥未来高等学校池袋キャンパス　2025年9月30日</li>
         <li>出演：ニコニコ超会議　2025年4月27日</li>
         <li>講演：「法で遊んでみよう」板橋区立教育科学館　2024年12月22日</li>
         <li>講演：神保町マボロシschool「倫獄先生の、法律入門」　2024年10月27日</li>
